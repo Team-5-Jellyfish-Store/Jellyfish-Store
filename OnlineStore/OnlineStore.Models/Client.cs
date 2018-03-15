8255fd71159f01427ca091aaaf294de36f009296
Unknown <marti_stamatov@abv.bg>
2018-03-15 13:57:58 +0200

    xfdz

---
+++ OnlineStore/OnlineStore.Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+}

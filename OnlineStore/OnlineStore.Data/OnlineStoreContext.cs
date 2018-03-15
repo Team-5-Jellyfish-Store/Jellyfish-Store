4f6a633ce065ac06cc55d2548336b6fe53340cc3
Martin Stamatov <marti_stamatov@abv.bg>
2018-03-15 14:02:50 +0200

    Connection string added

--- OnlineStore/OnlineStore.Data/OnlineStoreContext.cs
+++ OnlineStore/OnlineStore.Data/OnlineStoreContext.cs
@@ -1,9 +1,21 @@
-﻿using System.Data.Entity;
+﻿
+
+using OnlineStore.Models;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
 
 namespace OnlineStore.Data
 {
     public class OnlineStoreContext : DbContext
     {
+        public OnlineStoreContext() : base("OnlineStore")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {

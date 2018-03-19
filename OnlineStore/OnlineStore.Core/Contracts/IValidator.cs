0ced9ffc99e9dc694f43868de657192acf9c5470
Unknown <dimitar.petrov.311@gmail.com>
2018-03-19 16:54:49 +0200

    validations for register and login commands

--- OnlineStore/OnlineStore.Core/Contracts/IValidator.cs
+++ OnlineStore/OnlineStore.Core/Contracts/IValidator.cs
@@ -3,5 +3,13 @@
     public interface IValidator
     {
         bool IsValid(object obj);
+
+        string ValidateValue(string property, bool isRequired);
+
+        void ValidateEmail(string email);
+
+        void ValidatePassword(string password);
+
+        void ValidateLength(string property, int minLength, int maxLength);
     }
 }
--- OnlineStore/OnlineStore.Core/Providers/Validator.cs
+++ OnlineStore/OnlineStore.Core/Providers/Validator.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using OnlineStore.Core.Contracts;
-
+using System;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 
 namespace OnlineStore.Core.Providers
 {
@@ -14,6 +16,53 @@ namespace OnlineStore.Core.Providers
 
             var isValid = System.ComponentModel.DataAnnotations.Validator.TryValidateObject(obj, validationContext, validationResults, true);
             return isValid;
+        }
+
+        public string ValidateValue(string property, bool isRequired)
+        {
+            if (isRequired)
+            {
+                property = property != string.Empty ? property : throw new ArgumentException("The field is Required");
+            }
+            else
+            {
+                property = property != string.Empty ? property : null;
+            }
+
+            return property;
+        }
+
+        public void ValidateEmail(string email)
+        {
+            try
+            {
+                new MailAddress(email);
+            }
+            catch
+            {
+                throw new ArgumentException("Wrong email format!");
+            }
+
+        }
+
+        public void ValidatePassword(string password)
+        {
+            string passwordPattern = @"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+
+            if (!Regex.IsMatch(password, passwordPattern))
+            {
+                throw new ArgumentException("Password must be minimum eight characters, at least one letter and one number!");
+            }
+        }
+
+        public void ValidateLength(string property, int minLength, int maxLength)
+        {
+            int propLength = property.Length;
+
+            if (propLength < minLength || propLength > maxLength)
+            {
+                throw new ArgumentException($"Field length must be between {minLength} and {maxLength} symbols!");
+            }
         }
     }
 }

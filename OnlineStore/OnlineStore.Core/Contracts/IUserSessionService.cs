9271f5af5099fc6dae9149a3df4926ad8835c910
Unknown <dimitar.petrov.311@gmail.com>
2018-03-19 13:24:09 +0200

    user session modified

--- OnlineStore/OnlineStore.Core/Contracts/IUserSessionService.cs
+++ OnlineStore/OnlineStore.Core/Contracts/IUserSessionService.cs
@@ -4,7 +4,7 @@ namespace OnlineStore.Core.Contracts
 {
     public interface IUserSessionService
     {
-        User User { get; }
+        string GetLoggedUser();
 
         void SetLoggedUser(User user);
 

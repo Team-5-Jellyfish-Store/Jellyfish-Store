5c0da5b3aee4e418a2829d044c8d30ecda1e80cb
Unknown <dimitar.petrov.311@gmail.com>
2018-03-19 16:37:39 +0200

    register command validations on password, email etc

--- OnlineStore/OnlineStore.Core/Commands/RegisterClientCommand.cs
+++ OnlineStore/OnlineStore.Core/Commands/RegisterClientCommand.cs
@@ -6,6 +6,8 @@ using System;
 using System.Linq;
 using OnlineStore.Core.Contracts;
 using OnlineStore.Models.DataModels;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
 
 namespace OnlineStore.Core.Commands
 {
@@ -28,45 +30,57 @@ namespace OnlineStore.Core.Commands
         {
             this.writer.Write("Username: ");
             string username = this.reader.Read();
-            username = username != string.Empty ? username : throw new ArgumentException("Username is Required");
+            username = ValidateValue(username, true);
+
+            this.writer.Write("Email: ");
+            string email = this.reader.Read();
+            email = ValidateValue(email, true);
+            ValidateEmail(email);
+
+            this.writer.Write("Password: ");
+            string password = this.reader.Read();
+            password = ValidateValue(password, true);
+            ValidateLength(password, 8, 30);
+            ValidatePassword(password);
+
+            this.writer.Write("Confirm password: ");
+            string confirmedPassword = this.reader.Read();
+            confirmedPassword = ValidateValue(confirmedPassword, true);
+            ValidateLength(confirmedPassword, 8, 30);
+
+            this.writer.Write("First Name: ");
+            string firstName = this.reader.Read();
+            firstName = ValidateValue(firstName, false);
+
+            this.writer.Write("Last Name: ");
+            string lastName = this.reader.Read();
+            lastName = ValidateValue(lastName, false);
+
+            this.writer.Write("Address: ");
+            string address = this.reader.Read();
+            address = ValidateValue(address, true);
 
             if (this.context.Users.Any(x => x.Username == username))
             {
                 throw new ArgumentException("User with that username already exists!");
             }
 
-            this.writer.Write("Password: ");
-            string password = this.reader.Read();
-            password = password != string.Empty ? password : throw new ArgumentException("Password is Required");
-
-            this.writer.Write("Confirm password: ");
-            string confirmedPassword = this.reader.Read();
-
             if (password != confirmedPassword)
             {
                 throw new ArgumentException("Password not matching!");
             }
 
-            this.writer.Write("First Name: ");
-            string firstName = this.reader.Read();
-
-            this.writer.Write("Last Name: ");
-            string lastName = this.reader.Read();
-
-            this.writer.Write("Address: ");
-            string address = this.reader.Read();
-            address = address != string.Empty ? address : throw new ArgumentException("Address is Required");
+            password = this.hasher.CreatePassword(password);
 
             var addressFromDb = context.Addresses.Where(x => x.AddressText == address).FirstOrDefault()
                                 ?? throw new ArgumentNullException("Address not Found!");
-
-            password = this.hasher.CreatePassword(password);
 
             var newUser = new User()
             {
                 FirstName = firstName,
                 LastName = lastName,
                 Username = username,
+                EMail = email,
                 Password = password,
                 AddressId = addressFromDb.Id
             };
@@ -76,6 +90,53 @@ namespace OnlineStore.Core.Commands
             context.SaveChanges();
 
             return "User registered successfully!";
+        }
+
+        private void ValidateEmail(string email)
+        {
+            try
+            {
+                new MailAddress(email);
+            }
+            catch
+            {
+                throw new ArgumentException("Wrong email format!");
+            }
+
+        }
+
+        private void ValidatePassword(string password)
+        {
+            string passwordPattern = @"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+
+            if (!Regex.IsMatch(password, passwordPattern))
+            {
+                throw new ArgumentException("Password must be minimum eight characters, at least one letter and one number!");
+            }
+        }
+
+        private void ValidateLength(string property, int minLength, int maxLength)
+        {
+            int propLength = property.Length;
+
+            if (propLength < minLength || propLength > maxLength)
+            {
+                throw new ArgumentException($"Field length must be between {minLength} and {maxLength} symbols!");
+            }
+        }
+
+        private string ValidateValue(string property, bool isRequired)
+        {
+            if (isRequired)
+            {
+                property = property != string.Empty ? property : throw new ArgumentException("The field is Required");
+            }
+            else
+            {
+                property = property != string.Empty ? property : null;
+            }
+
+            return property;
         }
     }
 }

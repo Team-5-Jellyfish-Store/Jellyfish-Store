3d638a99ef50be3a5333ba6d178d3a4cd61f675a
Unknown <dimitar.petrov.311@gmail.com>
2018-03-20 10:21:26 +0200

    Services Assembly

---
+++ OnlineStore/OnlineStore.Logic/UserService.cs
@@ -0,0 +1,19 @@
+﻿using OnlineStore.Data.Contracts;
+
+namespace OnlineStore.Logic
+{
+    public class UserService
+    {
+        private readonly IOnlineStoreContext context;
+
+        public UserService(IOnlineStoreContext context)
+        {
+            this.context = context;
+        }
+
+        public void GetAllUsers()
+        {
+
+        }
+    }
+}

9af26b0440167e5db95eff9d1ba9f2c19280e6a1
Martin Stamatov <marti_stamatov@abv.bg>
2018-03-30 02:40:03 +0300

    Search and Print Commands Tests done

--- OnlineStore/OnlineStore.Tests/Commands/PrintOrdersReport/ExecuteThisCommand_Should.cs
+++ OnlineStore/OnlineStore.Tests/Commands/PrintOrdersReport/ExecuteThisCommand_Should.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using OnlineStore.Core.Commands;
+using OnlineStore.Core.Contracts;
+using OnlineStore.DTO.OrderModels.Constracts;
+using OnlineStore.Logic.Contracts;
+using OnlineStore.Providers.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +13,91 @@ using System.Threading.Tasks;
 
 namespace OnlineStore.Tests.Commands.PrintOrdersReport
 {
-    class ExecuteThisCommand_Should
+    [TestClass]
+    public class ExecuteThisCommand_Should
     {
+        [TestMethod]
+        public void Invoke_UserSession_CheckRights()
+        {
+            //Arrange
+            var stubOrderService = new Mock<IOrderService>();
+            var mockUserSession = new Mock<IUserSession>();
+            var stubPDFExporter = new Mock<IPDFExporter>();
+            var testedCommand = new PrintOrdersReportCommand(stubOrderService.Object, mockUserSession.Object, stubPDFExporter.Object);
+
+            //Act
+            testedCommand.ExecuteThisCommand();
+            //Assert
+            mockUserSession.Verify(x => x.HasAdminRights());
+        }
+
+        [TestMethod]
+        public void ReturnCorrect_WhenUserIsNotAdmin()
+        {
+            //Arrange
+            var stubOrderService = new Mock<IOrderService>();
+            var stubUserSession = new Mock<IUserSession>();
+            var stubPDFExporter = new Mock<IPDFExporter>();
+            var testedCommand = new PrintOrdersReportCommand(stubOrderService.Object, stubUserSession.Object, stubPDFExporter.Object);
+            var expected = "User must be admin or moderator in order to export data!";
+
+            //Act
+            stubUserSession.Setup(x => x.HasAdminRights()).Returns(false);
+            var actual = testedCommand.ExecuteThisCommand();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Invoke_OrderServiceGetAll()
+        {
+            //Arrange
+            var mockOrderService = new Mock<IOrderService>();
+            var stubUserSession = new Mock<IUserSession>();
+            stubUserSession.Setup(x => x.HasAdminRights()).Returns(true);
+            var stubPDFExporter = new Mock<IPDFExporter>();
+            var testedCommand = new PrintOrdersReportCommand(mockOrderService.Object, stubUserSession.Object, stubPDFExporter.Object);
+
+            //Act
+            var actual = testedCommand.ExecuteThisCommand();
+
+            //Assert
+            mockOrderService.Verify(x => x.GetAllOrders(), Times.Once);
+        }
+
+        [TestMethod]
+        public void Invoke_PDFExporterExportOrders()
+        {
+            //Arrange
+            var stubOrderService = new Mock<IOrderService>();
+            var stubUserSession = new Mock<IUserSession>();
+            stubUserSession.Setup(x => x.HasAdminRights()).Returns(true);
+            var mockPDFExporter = new Mock<IPDFExporter>();
+            var testedCommand = new PrintOrdersReportCommand(stubOrderService.Object, stubUserSession.Object, mockPDFExporter.Object);
+            var fakeCollection = new List<IOrderModel>().AsEnumerable();
+            //Act
+            testedCommand.ExecuteThisCommand();
+            //Assert
+            mockPDFExporter.Verify(x => x.ExportOrders(fakeCollection));
+        }
+
+        [TestMethod]
+        public void ReturnCorrect_WhenUserIsAdmin()
+        {
+            //Arrange
+            var stubOrderService = new Mock<IOrderService>();
+            var stubUserSession = new Mock<IUserSession>();
+            var stubPDFExporter = new Mock<IPDFExporter>();
+            var testedCommand = new PrintOrdersReportCommand(stubOrderService.Object, stubUserSession.Object, stubPDFExporter.Object);
+            var expected = "Exported orders successful";
+
+            //Act
+            stubUserSession.Setup(x => x.HasAdminRights()).Returns(true);
+            var actual = testedCommand.ExecuteThisCommand();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
---
+++ OnlineStore/OnlineStore.Tests/Commands/SearchCategory/ExecuteThisCommand_Should.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using OnlineStore.Core.Commands;
+using OnlineStore.Core.Contracts;
+using OnlineStore.DTO.ProductModels.Contracts;
+using OnlineStore.Logic.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Tests.Commands.SearchCategory
+{
+    [TestClass]
+    public class ExecuteThisCommand_Should
+    {
+        [TestMethod]
+        public void Invoke_WriterWriteLine()
+        {
+
+            //Arrange
+            var stubProductService = new Mock<IProductService>();
+            var stubReader = new Mock<IReader>();
+            var mockWriter = new Mock<IWriter>();
+            var testedCommand = new SearchCategoryCommand
+                (stubProductService.Object, stubReader.Object, mockWriter.Object);
+
+
+            //Act
+            testedCommand.ExecuteThisCommand();
+
+            //Assert
+            mockWriter.Verify(x => x.WriteLine(It.IsAny<string>()), Times.Once);
+
+        }
+
+        [TestMethod]
+        public void Invoke_ReaderRead()
+        {
+
+            //Arrange
+            var stubProductService = new Mock<IProductService>();
+            var mockReader = new Mock<IReader>();
+            var stubWriter = new Mock<IWriter>();
+            var testedCommand = new SearchCategoryCommand
+                (stubProductService.Object, mockReader.Object, stubWriter.Object);
+
+
+            //Act
+            testedCommand.ExecuteThisCommand();
+
+            //Assert
+            mockReader.Verify(x => x.Read(), Times.Once);
+
+        }
+
+        [TestMethod]
+        public void Invoke_ProductServiceGetProductByCategoryName()
+        {
+
+            //Arrange
+            var mockProductService = new Mock<IProductService>();
+            var stubReader = new Mock<IReader>();
+            var stubWriter = new Mock<IWriter>();
+            var testedCommand = new SearchCategoryCommand
+                (mockProductService.Object, stubReader.Object, stubWriter.Object);
+
+
+            //Act
+            testedCommand.ExecuteThisCommand();
+
+            //Assert
+            mockProductService.Verify(x => x.GetProductsByCategoryName(It.IsAny<string>()), Times.Once);
+
+        }
+
+        [TestMethod]
+        public void GetProductsName_IfMatchExist()
+        {
+
+            //Arrange
+            var stubProductService = new Mock<IProductService>();
+            var mockProduct = new Mock<IProductModel>();
+            var fakeCollection = new List<IProductModel> { mockProduct.Object }.AsEnumerable();
+
+            stubProductService.Setup(x => x.GetProductsByCategoryName(It.IsAny<string>()))
+                .Returns(fakeCollection);
+            var stubReader = new Mock<IReader>();
+            var stubWriter = new Mock<IWriter>();
+            var testedCommand = new SearchCategoryCommand
+                (stubProductService.Object, stubReader.Object, stubWriter.Object);
+
+
+            //Act
+            testedCommand.ExecuteThisCommand();
+
+            //Assert
+            mockProduct.Verify(x => x.Name, Times.Once);
+        }
+
+        [TestMethod]
+        public void GetProductsSellingPrice_IfMatchExist()
+        {
+
+            //Arrange
+            var stubProductService = new Mock<IProductService>();
+            var mockProduct = new Mock<IProductModel>();
+            var fakeCollection = new List<IProductModel> { mockProduct.Object }.AsEnumerable();
+
+            stubProductService.Setup(x => x.GetProductsByCategoryName(It.IsAny<string>()))
+                .Returns(fakeCollection);
+            var stubReader = new Mock<IReader>();
+            var stubWriter = new Mock<IWriter>();
+            var testedCommand = new SearchCategoryCommand
+                (stubProductService.Object, stubReader.Object, stubWriter.Object);
+
+
+            //Act
+            testedCommand.ExecuteThisCommand();
+
+            //Assert
+            mockProduct.Verify(x => x.SellingPrice, Times.Once);
+        }
+    }
+}

d3ce05c927955ac5cffe31b31476fc8ad880f2da
Unknown <dimitar.petrov.311@gmail.com>
2018-03-30 18:07:06 +0300

    Testing Logout Cmd

---
+++ OnlineStore/OnlineStore.Tests/Commands/Logout/Constructor_Should.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineStore.Core.Commands;
+using System;
+
+namespace OnlineStore.Tests.Commands.Logout
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void Throw_ArgumentNullException_When_UserSession_IsNull()
+        {
+            // Arrange
+            Action creatingLogoutCmd = () => new LogoutCommand(null);
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(creatingLogoutCmd);
+        }
+    }
+}
---
+++ OnlineStore/OnlineStore.Tests/Commands/Logout/ExecuteThisCommand_Should.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using OnlineStore.Core.Commands;
+using OnlineStore.Core.Contracts;
+using System;
+
+namespace OnlineStore.Tests.Commands.Logout
+{
+    [TestClass]
+    public class ExecuteThisCommand_Should
+    {
+        [TestMethod]
+        public void Throw_ArgumentException_When_NoUser_HasLoggedIn()
+        {
+            // Arrange
+            var userSessionStub = new Mock<IUserSession>();
+            var logoutCmd = new LogoutCommand(userSessionStub.Object);
+
+            userSessionStub
+                .Setup(us => us.HasSomeoneLogged())
+                .Returns(false);
+
+            Action executingLogoutCmd = () => logoutCmd.ExecuteThisCommand();
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentException>(executingLogoutCmd);
+        }
+
+        [TestMethod]
+        public void Invoke_UserSession_GetLoggetUsername_IfSomeOne_IsLoggedIn()
+        {
+            // Arrange
+            var mockUserSession = new Mock<IUserSession>();
+            var logoutCmd = new LogoutCommand(mockUserSession.Object);
+
+            mockUserSession
+                .Setup(us => us.HasSomeoneLogged())
+                .Returns(true);
+
+            // Act
+            logoutCmd.ExecuteThisCommand();
+
+            // Assert
+            mockUserSession.Verify(us => us.GetLoggedUserName(), Times.Once);
+        }
+
+        [TestMethod]
+        public void Invoke_UserSession_LogoutMethod_IfSomeOne_IsLoggedIn()
+        {
+            // Arrange
+            var mockUserSession = new Mock<IUserSession>();
+            var logoutCmd = new LogoutCommand(mockUserSession.Object);
+
+            mockUserSession
+                .Setup(us => us.HasSomeoneLogged())
+                .Returns(true);
+
+            // Act
+            logoutCmd.ExecuteThisCommand();
+
+            // Assert
+            mockUserSession.Verify(us => us.Logout(), Times.Once);
+        }
+    }
+}

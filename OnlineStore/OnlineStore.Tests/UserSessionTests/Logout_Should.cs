f40ceafaeb0d18169dd7e6e0f88fc9de0bfe4f3f
Unknown <dimitar.petrov.311@gmail.com>
2018-03-31 19:53:08 +0300

    Testing UserSession

---
+++ OnlineStore/OnlineStore.Tests/UserSessionTests/Logout_Should.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineStore.Tests.Mocks;
+
+namespace OnlineStore.Tests.UserSessionTests
+{
+    [TestClass]
+    public class Logout_Should
+    {
+        [TestMethod]
+        public void Set_LoggedUser_ToNull()
+        {
+            // Arrange
+            var userSession = new MockUserSession();
+
+            // Act
+            userSession.Logout();
+            var actualResult = userSession.ExposeLoggedUser();
+
+            // Assert
+            Assert.IsNull(actualResult);
+        }
+    }
+}
